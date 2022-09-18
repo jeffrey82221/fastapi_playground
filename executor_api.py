90a4534d7a449a492726a905360f56280cdaad7d
jeffrey82221 <jeffrey82221@gmail.com>
2022-09-18 16:33:12 +0800

    feature: multi replicas ray serving

--- executor_api.py
+++ executor_api.py
@@ -1,12 +1,30 @@
+"""
+How to setup executor_api? 
+pip install -r requirements.txt
+uvicorn executor_api:app --port 8080
+
+
+"""
+from ray import serve
 from fastapi import FastAPI
+import time
 
 app = FastAPI()
+@serve.deployment(
+    autoscaling_config={
+        "min_replicas": 1,
+        "max_replicas": 3,
+        "target_num_ongoing_requests_per_replica": 1,
+    }
+)
+@app.get("/")
+def func(item_id: str):
+    print('start func')
+    time.sleep(15)
+    print('end func')
+    return {"item_id:", item_id}
 
-from transformers import pipeline # A simple API for NLP tasks.
 
-nlp_model = pipeline("text-generation", model="gpt2") # Load the model.
-
-# The function below handles GET requests to the URL `/generate`.
-@app.get("/generate")
-def generate(query: str):
-    return nlp_model(query, max_length=50) # Output 50 words based on query.
+serve.run(
+    func.bind()
+)
