d9ced2d4d0bb4d551c5fbf1526943f851a33db4b
jeffrey82221 <jeffrey82221@gmail.com>
2022-09-24 12:08:47 +0000

    fix: compatibility on colab

--- porting.py
+++ porting.py
@@ -1,6 +1,7 @@
 import nest_asyncio
 from pyngrok import ngrok
 import uvicorn
+from executor_api import app
 ngrok.set_auth_token("296zATB6WYNOnhCSqlQJYyxwTor_69pPVvPvKTrskkTva7ok")
 ngrok_tunnel = ngrok.connect(9999)
 print('Public URL:', ngrok_tunnel.public_url)
