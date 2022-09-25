d09c0c27455e17cdacdabdaa1e4e840474746c35
jeffrey82221 <jeffrey82221@gmail.com>
2022-09-25 18:44:10 +0800

    doc: todo

--- test_colab_execution.py
+++ test_colab_execution.py
@@ -1,5 +1,31 @@
 """
 Testing executor_api
+
+TODO:
+- [ ] Build api that takes input and output pickle paths, and a function and do the following:
+    - [ ] extract elements from pickles on input paths
+    - [ ] send the elements to the function
+    - [ ] get the output from function
+    - [ ] save the output as pickle to the path!
+    - [ ] The API takes new input:
+        - {
+            'python_func': python_func,
+            'args_paths': input_paths,
+            'kwargs_paths': kwargs_paths,
+            'output_paths': output_paths,
+            'is_class_method': True/False
+        }
+    - [ ] If 'pickle' is not in the arg of input_paths , directly takes input from
+        API input
+    - [ ] If 'pickle' is not in the arg of output_paths, put the result into
+        the API output "return" field
+- [ ] Move here the Monad class, which automatically decorate `run` method as `binded_run`
+- [ ] Build a decorator (bind) that takes a function as input, and adapt it to the remote
+API
+    - [ ] It determine python_func via the source code of the func
+    - [ ] It determine args_paths & kwargs_paths via get_path of the args & kwargs ReturnObj
+    - [ ] It determine output_paths via the pre-build ReturnObjs' get_path
+        - [ ] ReturnObjs of output is build according to the Return __annotations__ of the func
 """
 from functools import wraps
 import requests
